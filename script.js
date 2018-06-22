5b8ae996d97aa948a4c93b746e93df6ca15ef5fb
lucie <nejezchlebovalucie@gmail.com>
2018-06-22 09:47:48 +0200

    first version

---
+++ script.js
@@ -0,0 +1,11 @@
+// $('#myModal').on('shown.bs.modal', function () {
+//   $('#myInput').trigger('focus')
+// })
+
+// 
+
+$(document).ready(function(){
+    $("#myBtn").click(function(){
+        $("#myModal").modal();
+    });
+});
