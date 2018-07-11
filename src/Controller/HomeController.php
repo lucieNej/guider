e2576815b33369e622c2ce187a85ff198cb666c9
annaS <dupa113@dupa.pl>
2018-07-11 23:59:21 +0200

    symfony added

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function homepage()
+    {
+        return $this->render('base.html.twig');
+    }
+
+}
---
+++ src/Controller/SearchController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SearchController extends AbstractController
+{
+    /**
+     * @Route("/search", name="search")
+     */
+    public function search()
+    {
+        return new Response('accueil.html is going to be here');
+    }
+}
